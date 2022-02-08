a31b18619ed7a63e0deab4b37b80577f253091c3
ks <92867218+ksIsCute@users.noreply.github.com>
2022-02-08 10:32:58 -0500

    Create discordstatus.py

---
+++ discordstatus.py
@@ -0,0 +1,5 @@
+await bot.change_presence(activity=discord.Game(name="a game")) # playing status (type 1)
+await bot.change_presence(activity=discord.Streaming(name="My Stream", url=my_twitch_url)) # streaming status (type 2)
+await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="a song")) # listening status (type 3)
+await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="a movie")) # watching status (type 4)
+await bot.change_presence(activity=discord.Activity(type=5, name=f"the hunger games")) # competing in status (type 5)
