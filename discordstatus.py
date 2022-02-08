f99ac90de7528fa2d0786ae8a764c7ff2b7be9a0
ks <92867218+ksIsCute@users.noreply.github.com>
2022-02-08 10:34:36 -0500

    Update discordstatus.py

--- discordstatus.py
+++ discordstatus.py
@@ -3,3 +3,8 @@ await bot.change_presence(activity=discord.Streaming(name="My Stream", url=my_tw
 await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="a song")) # listening status (type 3)
 await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="a movie")) # watching status (type 4)
 await bot.change_presence(activity=discord.Activity(type=5, name=f"the hunger games")) # competing in status (type 5)
+
+status=discord.Status.idle
+status=discord.Status.dnd # do not disturb
+status=discord.Status.invisible
+status=discord.Status.online
