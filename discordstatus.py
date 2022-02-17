8d7fbf9c8df7b21cdc3e74a41b4fbd5c0882d133
ks <92867218+ksIsCute@users.noreply.github.com>
2022-02-17 13:46:07 -0500

    Update discordstatus.py
    variable

--- discordstatus.py
+++ discordstatus.py
@@ -2,7 +2,9 @@ await bot.change_presence(activity=discord.Game(name="a game")) # playing status
 await bot.change_presence(activity=discord.Streaming(name="My Stream", url=my_twitch_url)) # streaming status (type 2)
 await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="a song")) # listening status (type 3)
 await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="a movie")) # watching status (type 4)
-await bot.change_presence(activity=discord.Activity(type=5, name=f"the hunger games")) # competing in status (type 5)
+await bot.change_presence(activity=discord.Activity(type=5, name="the hunger games")) # competing in status (type 5)
+#these all support fstrings and variables inside of them
+
 
 status=discord.Status.idle
 status=discord.Status.dnd # do not disturb
